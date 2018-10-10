398d98bd8a7df0657a6ac00bdc9fbcd683c73bf4
yunlin <zxc870830@gmail.com>
2018-10-10 12:27:53 +0800

    HW2

---
+++ test_p30/src/test_p30/test_p30.java
@@ -0,0 +1,16 @@
+package test_p30;
+
+public class test_p30 {
+
+	 public static void main(String[]args)
+	 {
+		 int i,j;
+		 for(i=0;i<5;i++)
+		 {
+			 for(j=0;j<3;j++)
+			 {
+				 System.out.println("i°é"+i+"j¬O"+j);
+			 }
+		 }
+	 }
+}
---
+++ test_p49/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author user
+ *
+ */
+module test_p49 {
+	exports test_p49;
+}
